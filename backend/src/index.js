3922fe4f5d04b162d9bd286dc45afd06e7cc1abd
hassamshaikh <hassamshaikh94@gmail.com>
2025-04-08 17:03:11 +0500

    chat app started

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(5001, () => {
+  console.log("server is running on PORT " + port);
+  connectDB();
+});
